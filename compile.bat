942b07e73efd5c614a8cd53c782d0c1b293b8a90
catherine <catherine@mail.com>
2014-06-06 00:03:51 +0300

    Publish/subscribe.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,8 @@
-csc /out:Send.exe /r:"RabbitMQ.Client.dll" Send.cs
-csc /out:Receive.exe /r:"RabbitMQ.Client.dll" Receive.cs
+rem csc /out:Send.exe /r:"RabbitMQ.Client.dll" Send.cs
+rem csc /out:Receive.exe /r:"RabbitMQ.Client.dll" Receive.cs
 
-csc /out:NewTask.exe /r:"RabbitMQ.Client.dll" NewTask.cs
-csc /out:Worker.exe /r:"RabbitMQ.Client.dll" Worker.cs
+rem csc /out:NewTask.exe /r:"RabbitMQ.Client.dll" NewTask.cs
+rem csc /out:Worker.exe /r:"RabbitMQ.Client.dll" Worker.cs
+
+csc /r:"RabbitMQ.Client.dll" EmitLogs.cs
+csc /r:"RabbitMQ.Client.dll" ReceiveLogs.cs
