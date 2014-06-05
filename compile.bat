c810b0a6dbd39343d76063107f3a30c0d6f5ed59
catherine <catherine@mail.com>
2014-06-06 00:11:13 +0300

    Routing.

--- compile.bat
+++ compile.bat
@@ -4,5 +4,8 @@ rem csc /out:Receive.exe /r:"RabbitMQ.Client.dll" Receive.cs
 rem csc /out:NewTask.exe /r:"RabbitMQ.Client.dll" NewTask.cs
 rem csc /out:Worker.exe /r:"RabbitMQ.Client.dll" Worker.cs
 
-csc /r:"RabbitMQ.Client.dll" EmitLogs.cs
-csc /r:"RabbitMQ.Client.dll" ReceiveLogs.cs
+rem csc /r:"RabbitMQ.Client.dll" EmitLogs.cs
+rem csc /r:"RabbitMQ.Client.dll" ReceiveLogs.cs
+
+csc /r:"RabbitMQ.Client.dll" EmitLogDirect.cs
+csc /r:"RabbitMQ.Client.dll" ReceiveLogsDirect.cs
